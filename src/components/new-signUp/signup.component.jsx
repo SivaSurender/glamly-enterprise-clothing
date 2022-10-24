b5e974415c996e8885c507624a4317a11a91dd5e
SivaSurender <e.surender14@gmail.com>
2022-10-24 15:23:36 +0530

    replacing the signup placeholder with usestate funtionality

--- src/components/new-signUp/signup.component.jsx
+++ src/components/new-signUp/signup.component.jsx
@@ -1,21 +1,61 @@
-import React from "react";
+import { useState } from "react";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
 const NewSignUp = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+  console.log(formFields);
+
+  const formHandleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
   return (
     <div>
       <h1>SignUp with your email </h1>
       <form onSubmit={() => {}}>
         <label>Display Name</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          onChange={formHandleChange}
+          name="displayName"
+          value={displayName}
+        />
 
         <label>Email Address</label>
-        <input type="email" required />
+        <input
+          type="email"
+          required
+          onChange={formHandleChange}
+          name="email"
+          value={email}
+        />
 
         <label>Password</label>
-        <input type="password" required />
+        <input
+          type="password"
+          required
+          onChange={formHandleChange}
+          name="password"
+          value={password}
+        />
 
         <label>Confirm Password</label>
-        <input type="password" required />
+        <input
+          type="password"
+          required
+          onChange={formHandleChange}
+          name="confirmPassword"
+          value={confirmPassword}
+        />
 
         {/* button to submit the form as whole after validation from above label required fields
               then submitted through on submit handler attached in the form at the top */}
